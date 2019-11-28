d1ea914c47e89cd8290cd20933819c7ec0ec76f5
Jarod Davis <jarod.davis37@gmail.com>
2019-11-28 02:27:27 -0600

    Added Restroom with basic teleporting.

---
+++ SLCR/Assets/RestRoomTeleport.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestRoomTeleport : MonoBehaviour
+{
+    public Transform Destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = Destination.transform.position;
+        other.transform.position = Destination.transform.position;
+    }
+    
+}
