0e7813874558c561a547bc8f70725a3e19cf7c82
Jarod Davis <jarod.davis37@ku.edu>
2019-12-05 01:16:03 -0600

    Added end state to trap room, Lights to maze.

---
+++ SLCR/Assets/DeathPlane.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlane : MonoBehaviour
+{
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Player.GetComponent<PlayerController>().ChangeHealth(-100);
+        }        
+    }
+}
